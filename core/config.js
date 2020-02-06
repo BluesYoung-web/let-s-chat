eca065fc3ad060aead6d67a46b6906314a96d8e7
BluesYoung-web <15171255945@163.com>
2020-02-06 14:41:28 +0800

    write jsdoc

--- core/config.js
+++ core/config.js
@@ -1,6 +1,11 @@
+/**
+ * 配置文件
+ * @author 张扬
+ * @copyright http://github.com/BluesYoung-web
+ * @type {string} httpUrl http请求服务器地址
+ * @type {string} websocketUrl websocket服务器地址
+ */
 export default{
-    // 数据服务器地址
 	httpUrl:'http://www.bluesyoung-web.com',
-	// websocket服务器地址
 	websocketUrl:'ws://www.bluesyoung-web.com:8848',
 }

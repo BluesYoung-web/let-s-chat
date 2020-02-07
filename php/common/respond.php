b01b36e1a2f90b0ea9dc740c7f6db3494df56ff0
BluesYoung-web <15171255945@163.com>
2020-02-07 09:19:18 +0800

    添加doc注释

--- php/common/respond.php
+++ php/common/respond.php
@@ -1,8 +1,15 @@
 <?php
     /**
-     * @param $status 状态码
-     * @param $data (js的对象，PHP的索引数组)
-     * @param $msg (返回的提示信息)
+     * 用于http请求返回
+     * @author 张扬
+     * @copyright http://github.com/BluesYoung-web
+     */
+
+    /**
+     * 用于http请求返回的函数
+     * @param number $status 状态码
+     * @param array $data (js的对象，PHP的索引数组)
+     * @param string $msg (返回的提示信息)
      */
     function respond($status, $data, $msg){
         if(!$data){

bf051ed9eaa2ff380620df9347dfa39d4d04d418
BluesYoung-web <15171255945@163.com>
2020-02-07 16:57:57 +0800

    上传的后端文件

--- php/api/upload.php
+++ php/api/upload.php
@@ -1,6 +1,16 @@
 <?php
-    include_once('../common/redis.php');
-    $tk = $redis -> get('token');
-    $sign = md5("754433271".$tk);
-    echo $tk."\n".$sign;
+    $f=$_GET['fileType'];
+    // 获取上传文件对应的字典（对象）
+    $fileInfo = $_FILES[$f.''];
+    // 获取上传文件的名称
+    $fileName = $fileInfo["name"];
+    
+    // 获取上传文件保存的临时路径
+    $filePath = $fileInfo["tmp_name"];
+
+    // 将图片存到服务器
+    move_uploaded_file($filePath, "../".$f."/".$fileName);
+    // $new_avatar="http://117.78.0.214/".$f."/".$fileName;
+    $new_avatar="http://127.0.0.1/".$f."/".$fileName;
+    echo $new_avatar;
 ?>

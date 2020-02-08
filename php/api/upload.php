434d2b533685d3efe7f29bb8bc952ae07e93e4c4
BluesYoung-web <15171255945@163.com>
2020-02-08 15:56:53 +0800

    优化上传逻辑

--- php/api/upload.php
+++ php/api/upload.php
@@ -4,18 +4,23 @@
     // var_dump($_FILES);
     // 获取上传文件对应的字典（对象）
     $fileInfo = $_FILES[$f.''];
-    // 获取上传文件的名称
-    $fileName = $fileInfo["name"];
-    
-    // 获取上传文件保存的临时路径
-    $filePath = $fileInfo["tmp_name"];
+    // 上传出错
+    if ($fileInfo['error'] > 0) {
+        respond(-1, $fileInfo['error'], "文件上传出错");
+    } else {
+        // 获取上传文件的名称
+        $fileName = $fileInfo["name"];
+        
+        // 获取上传文件保存的临时路径
+        $filePath = $fileInfo["tmp_name"];
 
-    // 将文件存到服务器
-    move_uploaded_file($filePath, "../".$f."/".$fileName);
-    $url = "http://117.78.0.214/".$f."/".$fileName;
-    
-    $arr = [];
-    $arr['url'] = $url;
+        // 将文件存到服务器
+        move_uploaded_file($filePath, "../".$f."/".$fileName);
+        $url = "http://117.78.0.214/".$f."/".$fileName;
+        
+        $arr = [];
+        $arr['url'] = $url;
 
-    respond(0, $arr, "文件上传成功");
+        respond(0, $arr, "文件上传成功");
+    }
 ?>

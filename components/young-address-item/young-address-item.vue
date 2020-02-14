31295a169bf0049267685c8bbaad5cf70fd5c07b
BluesYoung-web <15171255945@163.com>
2020-02-14 10:59:08 +0800

    编写好友列表组件

---
+++ components/young-address-item/young-address-item.vue
@@ -0,0 +1,44 @@
+<template>
+	<view>
+		<view class="flex flex-direction-row flex-vc flex-jsb width-750 height-120 pd-lr30" v-for="(item, index) in friendsList" :key="index">
+			<image class="width-80 height-80 bd-rd50" :src="item.avatar" mode="aspectFill" @tap="toFriendInfo(item)"></image>
+			<view class="width-600 flex flex-direction-row flex-jsb pd-lr20 h120 line-h120 bd-bt-gainsboro" @tap="toFriendInfo(item)">
+				<text class="ft-32">{{item.nick}}</text>
+				<text v-if="item.online" class="ft-28 color-f9aa33">在线</text>
+				<text v-else class="ft-28 color-999">离线</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		/**
+		 * 组件名称
+		 */
+		name: 'addressItem',
+		/**
+		 * 组件属性
+		 */
+		props: {
+			friendsList: {
+				type: Array,
+				default(){
+					return []
+				}
+			}
+		},
+		methods:{
+			/**
+			 * 点击进入好友详情页
+			 */
+			toFriendInfo(item){
+				this.$emit('toFriendInfo', item);
+			}
+		}
+	}
+</script>
+
+<style>
+	@import url("./common.css");
+</style>

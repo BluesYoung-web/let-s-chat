7b5bfa87e39de0ae9dc76f710f1d0eb46839752d
BluesYoung-web <15171255945@163.com>
2020-02-18 11:05:04 +0800

    新增组件

---
+++ components/young-icon-list/young-icon-list.vue
@@ -0,0 +1,50 @@
+<!-- 个人页列表组件 -->
+<template>
+	<view>
+		<view @click="btnClick" class="bg-fff pd-lr20 width-750 height-100  flex flex-direction-row flex-vc flex-jsb">
+			<view class="h100 line-h100 flex flex-vc">
+				<image class="width-50 height-50" :src="iconUrl" mode=""></image>
+				<text class="pd-lt10 ft-30 color-333">{{title}}</text>
+			</view>
+			<view class="flex flex-ac">
+				<image class="rightIcon" src="/static/img/arrow-right.png" mode=""></image>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'iconList',
+		props: {
+			iconUrl: {
+				type: String,
+				default: '/static/img/message.png',
+				// required: true
+			},
+			title: {
+				type: String,
+				default: '默认信息',
+				// required:true
+			},
+		},
+		methods: {
+			btnClick() {
+				this.$emit("click");
+			},
+			loadMore(e) {
+				this.$emit("click");
+			}
+		}
+	}
+</script>
+
+<style>
+	/* 引入公共样式 */
+	@import url("./common.css");
+	.rightIcon{
+		height: 30upx;
+		width: 30upx;
+		margin-left: 30upx;
+	}
+</style>
---
+++ components/young-search-input/young-search-input.vue
@@ -0,0 +1,93 @@
+<!-- 输入框组件 可以设置背景色、宽度、圆角、边框、占位符 -->
+<template>
+	<view>
+		<view class="search relative" :style="{width: width}">
+			<span class="iconfont absolute searchIcon">&#xe6a3;</span>
+			<input v-model="inputMsg" type="text" @input="$emit('getInputMsg', $event.target.value)" class="input" value="" :placeholder="placeholder" :style="{'border-radius': bdRadius, border:border, 'background-color':backgroundColor}" />
+			<span @tap="clear" v-show="inputMsg.length != 0" class="iconfont absolute clearIcon">&#xe609;</span>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'searchInput',
+		props: {
+			//输入框宽度
+			width: {
+				type: String,
+				default: "600upx"
+			},
+			//圆角
+			bdRadius:{
+				type: String,
+				default: "10upx"
+			},
+			//边框
+			border:{
+				type: String,
+				default: "1px solid #D5D5D6"
+			},
+			//占位符
+			placeholder: {
+				type: String,
+				default: "搜索"
+				// required: true
+			},
+			backgroundColor:{
+				type: String,
+				default: "#ffffff"
+			}
+		},
+		data() {
+			return {
+				inputMsg:'',
+			}
+		},
+		methods: {
+			clear(){
+				this.inputMsg = '';
+				this.$emit('getInputMsg', this.inputMsg);
+			},
+		}
+	}
+</script>
+
+<style>
+	/* 引入公共样式 */
+	@import url("./common.css");
+	/* 引入字体图标 */
+	@font-face {
+	  font-family: 'iconfont';
+	  src: url('/static/ttf/iconfont.eot');
+	  src: url('/static/ttf/iconfont.eot?#iefix') format('embedded-opentype'),
+	      url('/static/ttf/iconfont.woff2') format('woff2'),
+	      url('/static/ttf/iconfont.woff') format('woff'),
+	      url('/static/ttf/iconfont.ttf') format('truetype'),
+	      url('/static/ttf/iconfont.svg#iconfont') format('svg');
+	}
+	.iconfont {
+	  font-family: "iconfont" !important;
+	  font-size: 16px;
+	  font-style: normal;
+	  -webkit-font-smoothing: antialiased;
+	  -moz-osx-font-smoothing: grayscale;
+	}
+	.input {
+		padding-left: 64upx;
+		width: 100%;
+		height: 80upx;
+	}
+	.searchIcon {
+		top: 24upx;
+		left: 24upx;
+		width: 30upx;
+		height: 30upx;
+	}
+	.clearIcon {
+		top: 24upx;
+		right: 20upx;
+		width: 36upx;
+		height: 36upx;
+	}
+</style>

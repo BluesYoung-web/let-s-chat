9b880a290838ba2063567c9212e54640f7b7adf1
BluesYoung-web <15171255945@163.com>
2020-03-04 17:10:36 +0800

    完善HTTP服务器

--- node/server/httpServer.js
+++ node/server/httpServer.js
@@ -8,10 +8,13 @@
  * 使用express框架
  */
 const express = require('express');
-const app = express();
 const path = require('path');
 const fs = require('fs');
 const morgan = require('morgan');
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
+
+const app = express();
 
 /**
  * 创建日志写入流
@@ -26,15 +29,23 @@ const login = require('./routes/login');
 const upload = require('./routes/upload');
 
 /**
- * 日志模块
+ * 日志模块...
  */
+app.use(morgan('dev'));
 app.use(morgan('combined', { stream: accessLogStream }));
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 /**
  * 托管静态文件，顺序执行，优先级高
  */
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ * 模板页面
+ */
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 /**
  * 根目录路由
  */
@@ -49,6 +60,25 @@ app.use('/login', login);
  * 文件上传接口
  */
 app.use('/upload', upload);
+
+/**
+ * 404
+ */
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+/**
+ * 错误显示
+ */
+app.use(function(err, req, res, next) {
+    // 仅开发模式显示
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // 渲染错误页面
+    res.status(err.status || 500);
+    res.render('error');
+});
 /**
  * 暴露
  */

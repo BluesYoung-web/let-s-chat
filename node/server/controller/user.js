1bb44844ff18d8f24161e122de27da6dab8d76d6
BluesYoung-web <15171255945@163.com>
2020-03-05 17:07:39 +0800

    实现用户登录接口

---
+++ node/server/controller/user.js
@@ -0,0 +1,117 @@
+/**
+ * 用户模块
+ * @author 张扬
+ * @copyright http://github.com/BluesYoung-web
+ */
+
+/**
+ * 引入mysql数据库查询方法
+ */
+const {mysqlQuery} = require('../database/conn');
+/**
+ * 引入操作redis的对象
+ */
+const {myredis} = require('../database/conn');
+/**
+ * 制作token的函数
+ */
+const makeToken = function(){
+    const md5 = require('md5');
+    const sha1 = require('sha1');
+    const {key} = require('../config');
+    return md5(key + sha1(Date.parse(new Date())));
+}
+/**
+ * 用户登录
+ * @param {string} tel 用户手机号
+ * @param {string} wxid 用户微信id
+ */
+const login = function(tel, wxid){
+    let search;
+    if (tel != '') {
+        search = `select * from user where tel = ${tel};`;
+    } else if (wxid != ''){
+        search = `select * from user where wxid = '${wxid}';`;
+    }
+    let combin = `update user set wxid = '${wxid}' where tel = ${tel};`;
+    let wxReg = `insert into user(tel,wxid) values(${tel},'${wxid}');`;
+    let telReg = `insert into user(tel) values(${tel});`
+    let token = makeToken();
+    const {respondProcess} = require('../core/tools');
+    return new Promise((resolve, reject) => {
+        mysqlQuery(search).then((data) => {
+            if (data.length != 0) {
+                // 如果是老用户
+                if (tel && wxid != '') {
+                    // 首次使用微信登录，绑定手机号，合并现有账号
+                    mysqlQuery(combin).then((dt) => {
+                        mysqlQuery(search).then((data) => {
+                            data = data.pop();
+                            data.token = token;
+                            let tp = {
+                                bnew: 0,
+                                user: data
+                            }
+                            // 存储token
+                            myredis.set(data.uid + '_token', token);
+                            resolve(respondProcess(0, tp, '合并现有账号成功'));
+                        });
+                    });
+                } else {
+                    data = data.pop();
+                    data.token = token;
+                    let tp = {
+                        bnew: 0,
+                        user: data
+                    }
+                    // 存储token
+                    myredis.set(data.uid + '_token', token);
+                    resolve(respondProcess(0, tp, '用户登录成功'));
+                }
+            } else {
+                // 新用户
+                if (tel && wxid) {
+                    // 新用户使用微信登录
+                    mysqlQuery(wxReg).then((dt) => {
+                        mysqlQuery(search).then((data) => {
+                            data = data.pop();
+                            data.token = token;
+                            let tp = {
+                                bnew: 1,
+                                user: data
+                            }
+                            // 存储token
+                            myredis.set(data.uid + '_token', token);
+                            resolve(respondProcess(0, tp, '微信注册成功'));
+                        });
+                    });
+                } else if(tel) {
+                    // 新用户使用手机号登录
+                    mysqlQuery(telReg).then((dt) => {
+                        mysqlQuery(search).then((data) => {
+                            data = data.pop();
+                            data.token = token;
+                            let tp = {
+                                bnew: 1,
+                                user: data
+                            }
+                            // 存储token
+                            myredis.set(data.uid + '_token', token);
+                            resolve(respondProcess(0, tp, '手机号注册成功'));
+                        });
+                    });
+                } else {
+                    resolve(respondProcess(-1, {}, '未绑定手机号'));
+                }
+            }
+        });
+    });
+}
+
+
+/**
+ * 暴露
+ */
+module.exports = {
+    login
+}

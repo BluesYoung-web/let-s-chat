e041f6041acceb75cb78d1ccddb835cf89be4a52
BluesYoung-web <15171255945@163.com>
2020-03-05 17:06:22 +0800

    后端配置文件

---
+++ node/server/config.js
@@ -0,0 +1,13 @@
+/**
+ * 配置文件
+ * @author 张扬
+ * @copyright http://github.com/BluesYoung-web
+ */
+
+module.exports = {
+    dbName: 'lailiao',
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    key: 'www.bluesyoung-web.com'
+}

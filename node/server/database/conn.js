55737f397d1525197f925d29ef10c5b15f3c2241
BluesYoung-web <15171255945@163.com>
2020-03-05 17:08:21 +0800

    实现三个数据库的连接与操作

--- node/server/database/conn.js
+++ node/server/database/conn.js
@@ -5,63 +5,84 @@
  */
 
 /**
- * 连接MongoDB数据库的函数
- * @param {string} dbName 数据库名
+ * 引入配置文件
  */
-const mongoConnect = function(dbName) {
-    const mongoose = require('mongoose');
-    let str = `mongodb://127.0.0.1:27017/${dbName}`;
-    mongoose.connect(str, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }, (err) => {
-        if (err) {
-            throw err;
-        } else{
-            console.log("数据库连接成功");
-        }
-    });
-}
+const config = require('../config');
 
 /**
- * 连接MySQL数据库的函数
- * @param {string} dbName 数据库名
+ * 连接MongoDB数据库
  */
-const mysqlConnect = function(dbName) {
+const mongoose = require('mongoose');
+let str = `mongodb://127.0.0.1:27017/${config.dbName}`;
+mongoose.connect(str, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, (err) => {
+    if (err) {
+        throw err;
+    } else{
+        console.log("MongoDB数据库连接成功");
+    }
+});
+/**
+ * mysql查询方法（暴露）
+ */
+const mysqlQuery = async function(sql){
     const mysql = require('mysql');
-    const connection = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'root',
-        password : '123456',
-        database : dbName
+    /**
+     * 创建mysql连接池
+     */
+    const pool = mysql.createPool({
+        host     : config.host,
+        user     : config.user,
+        password : config.password,
+        database : config.dbName
     });
-    connection.connect();
-    return connection;
+    return await new Promise((resolve, reject) => {
+        /**
+         * 从连接池创建连接
+         */
+        pool.getConnection((err, connection) => {
+            if (err) {
+                reject(err);
+            } else {
+                connection.query(sql, (err, data) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(data);
+                    }
+                    /**
+                     * 查询结束，释放连接
+                     */
+                    connection.release();
+                });
+            }
+        });
+    }).catch((err) => {
+        throw err;
+    });
 }
 
 /**
- * 连接Redis数据库的函数
+ * 连接Redis数据库，暴露操作对象
  */
-const redisConnect = function() {
-    const mongoose = require('mongoose');
-    let str = `mongodb://127.0.0.1:27017/${dbName}`;
-    mongoose.connect(str, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }, (err) => {
-        if (err) {
-            throw err;
-        } else{
-            console.log("数据库连接成功");
-        }
-    });
-}
+const redis = require('redis');
+const client = redis.createClient();
+// client.auth(config.password, () => {});
+client.on('error', (err) => {
+    console.log('redis数据库连接出错：\n' + err);
+});
+client.on('connect', () => {
+    console.log('redis数据库连接成功');
+});
+const MyRedis = require('../model/redis');
+const myredis = new MyRedis(client);
 
 /**
  * 暴露
  */
 module.exports = {
-    mongoConnect,
-    mysqlConnect,
-    redisConnect
+    mysqlQuery,
+    myredis
 };

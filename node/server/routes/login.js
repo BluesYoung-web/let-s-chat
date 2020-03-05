511119bc57b263c7180fc046a9f443b493b5c83f
BluesYoung-web <15171255945@163.com>
2020-03-05 17:09:17 +0800

    实现登录接口

--- node/server/routes/login.js
+++ node/server/routes/login.js
@@ -8,13 +8,27 @@ const express = require('express');
 
 const router = express.Router();
 
+const {login} = require('../controller/user');
+
 router.get('/', (req, res) => {
-    res.send('<h1>登陆路由</h1>')
+    // res.send('<h1>登陆路由</h1>');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    login(15171255945, '').then((data) => {
+        res.send(data);
+    });
 });
 
 router.post('/', (req, res) => {
-
+    let tel = req.body.tel || '';
+    let wxid = req.body.wxid || '';
+    // 设置跨域响应头
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    login(tel, wxid).then((data) => {
+        res.send(data);
+    });
 });
+
 
 /**
  * 暴露

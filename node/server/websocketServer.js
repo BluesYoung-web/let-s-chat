1192852eb6d131288abb565e1b26cd8b614ae83a
BluesYoung-web <15171255945@163.com>
2020-03-11 15:35:57 +0800

    服务器代码优化

--- node/server/websocketServer.js
+++ node/server/websocketServer.js
@@ -8,10 +8,23 @@
  * 引入websocket模块
  */
 const ws = require('nodejs-websocket');
+const Socket = require('./model/socket');
 /**
  * websocket连接参数分离
  */
 const {paramsSeparate} = require('./core/tools');
+const {resFormat} = require('./core/tools');
+
+/**
+ * 创建websocket服务器
+ */
+let websocketServer = ws.createServer((conn) => {
+    let {sign, uid} = {...paramsSeparate(conn.path)};
+    /**
+     * 连接建立之后立即验证token
+     */
+    tokenCheck(sign, uid, conn);
+});
 
 /**
  * token验证专用
@@ -26,10 +39,33 @@ const tokenCheck = function(sign, uid, conn){
         let token = data;
         if (md5(uid + token) == sign) {
             // 验证成功
-            conn.sendText('token验证成功');
+            conn.on('text', (str) =>{
+                str = JSON.parse(str);
+                let socket = new Socket(uid, conn, websocketServer);
+                socket.msgProcess(str);
+            });
+            conn.on('close', (reason) => {
+                console.log('socket服务器关闭'+reason);
+            });
+            conn.on('error', (reason) => {
+                console.log('服务器异常关闭'+reason);
+            });
+            let str = resFormat({
+                status: 0,
+                data: {
+                    msg: 'token验证成功'
+                }
+            });
+            conn.sendText(str);
         } else {
             // 验证失败
-            conn.sendText('token验证失败');
+            let str = resFormat({
+                status: -1,
+                data: {
+                    msg: 'token验证失败'
+                }
+            });
+            conn.sendText(str);
             setTimeout(() => {
                 conn.close();
             }, 1000);
@@ -37,19 +73,5 @@ const tokenCheck = function(sign, uid, conn){
     })
 }
 
-let websocketServer = ws.createServer((conn) => {
-    let {sign, uid} = {...paramsSeparate(conn.path)};
-    tokenCheck(sign, uid, conn);
-    conn.on('text', (str) =>{
-        console.log('msg:'+str);
-        conn.sendText(str);
-    });
-    conn.on('close', (code, reason) => {
-        console.log('socket服务器关闭'+reason);
-    });
-    conn.on('error', (code, reason) => {
-        console.log('服务器异常关闭'+reason);
-    });
-});
 
 module.exports = websocketServer;
